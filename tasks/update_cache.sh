fbea780893065fdf1f6a5faaa20972025eaa8f88
Marc McKinley <marcmckinley@me.com>
2020-05-10 14:37:44 +0100

    added cache standalone task and updated readme

---
+++ tasks/update_cache.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#Variables containing output messages
+updatecache='*******Updating Cache*******'
+cleancache='*******Cleaning Cache*******'
+
+
+#Checking cache input to determine which type of cache action to perform
+#cache: [update, clean]
+if [[ "$PT_cache" == "update" ]]; then
+    echo -e "\n$updatecache\n"
+    /usr/bin/yum clean expire-cache
+    echo ""
+elif [[ "$PT_cache" == "clean" ]]; then
+    echo -e "\n$cleancache\n"
+    /usr/bin/yum clean all
+    echo ""
+    fi
